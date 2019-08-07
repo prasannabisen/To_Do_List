2b5a96c7b2c66c2725e24e5a5686001bdbe1c2df
montu <montu@gmail.com>
2019-08-07 11:10:44 +0530

    .

---
+++ server.js
@@ -0,0 +1,9 @@
+window.onload=function()
+{
+    const input=document.getElementById('input')
+    const button=document.getElementById('button')
+    console.log(button)
+    button.addEventListener('click',()=>{
+        console.log('clicked')
+    })
+}

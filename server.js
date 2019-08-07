d859fe7e656ec17d862e804102674a9c433d6a0d
montu <montu@gmail.com>
2019-08-07 19:51:20 +0530

    .

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
+import { checkServerIdentity } from "tls";
+
 let task=[{
     todo:"work",
-    strike:true
+    strike:false
 }]
+
+function check(task){
+    for(let i=0;i<task.length;i++)
+    {
+        if(task[i].strike)
+        {
+            html+=`<li class="strike">${task[i].todo}</li>`
+        }
+        else{
+            html+=`<li>${task[i].todo}<li>`
+        }
+    }
+}
+
+function fun(){
+    const ol=document.getElementById('ol')
+    ol.innerHTML=check(task)
+    input.value=""
+}
 
 window.onload=function(){
     const button=document.getElementById('button')
@@ -9,7 +30,9 @@ window.onload=function(){
         const input=document.getElementById('input')
         task.push({
             todo:input.value,
-            strike:false
+            strike:true
         }) 
+        console.log(task)
+        fun()
     })
 }

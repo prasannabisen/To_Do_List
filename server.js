be85d1198cf652236956326f3ded5f4e516baff0
montu <montu@gmail.com>
2019-08-07 16:58:35 +0530

    .

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-const button=document.getElementById('button')
-const input=document.getElementById('input')
-console.log(button)
-button.onclick=function(){
-    console.log('kk');
+let task=[{
+    todo:"work",
+    strike:false
+}]
+
+window.onload=function(){
+    const button=document.getElementById('button')
+    button.addEventListener('click',function(){
+        const input=document.getElementById('input')
+        task.push({
+            todo:input.value,
+            strike:false
+        }) 
+    })
 }
